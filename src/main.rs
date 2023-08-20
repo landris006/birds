ad9ce832cb9c6c3e1f27cc304361e7379cba8207
landris006 <laczko.andris007@gmail.com>
2023-08-20 11:35:50 +0200

    improved behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowMode};
 mod systems;
 use systems::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // resolution: WindowResolution::new(1920., 1080.),
+                mode: WindowMode::BorderlessFullscreen,
+                ..default()
+            }),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
--- src/systems.rs
+++ src/systems.rs
@@ -1,5 +1,5 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
-const BOUNDS: Vec2 = Vec2::new(800., 400.);
+use bevy::{core_pipeline::bloom::BloomSettings, prelude::*, sprite::MaterialMesh2dBundle};
+const BOUNDS: Vec2 = Vec2::new(1800., 900.);
 const BIRDS_TO_SPAWN: i32 = 2000;
 
 pub fn setup(
@@ -7,21 +7,42 @@ pub fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
-
-    for i in 0..BIRDS_TO_SPAWN {
-        commands
-            .spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(shape::RegularPolygon::new(10., 3).into()).into(),
-                transform: Transform::from_rotation(Quat::from_rotation_z(0.5 * i as f32)),
-                material: materials.add(ColorMaterial::from(Color::BLACK)),
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true,
                 ..default()
-            })
-            .insert(Bird {
-                speed: 75.,
-                rotation_speed: 2.,
-            });
-    }
+            },
+
+            ..default()
+        },
+        BloomSettings::default(),
+    ));
+
+    let mesh_handle = meshes.add(shape::RegularPolygon::new(10., 3).into());
+    let material_handle = materials.add(ColorMaterial::from(Color::hsl(180., 1.0, 0.5)));
+
+    let batch = (0..BIRDS_TO_SPAWN).map(move |i| {
+        (
+            MaterialMesh2dBundle {
+                mesh: Handle::clone(&mesh_handle).into(),
+                material: Handle::clone(&material_handle),
+                transform: Transform::from_translation(Vec3::new(
+                    (i as f32 / BIRDS_TO_SPAWN as f32) * BOUNDS.x - BOUNDS.x / 2.,
+                    (i as f32 / BIRDS_TO_SPAWN as f32) * BOUNDS.x - BOUNDS.x / 2.,
+                    0.,
+                )),
+                ..default()
+            },
+            Bird {
+                speed: 100.,
+                rotation_speed: 1.,
+                vision_range: 100.,
+            },
+        )
+    });
+
+    commands.spawn_batch(batch);
 }
 
 pub fn move_birds(time: Res<Time>, mut query: Query<(&mut Transform, &Bird)>) {
@@ -41,55 +62,73 @@ pub fn draw_gizmos(mut gizmos: Gizmos) {
     );
 }
 
-pub fn rotate_birds(time: Res<Time>, mut query: Query<(Entity, &mut Transform, &Bird)>) {
-    let birds = query
-        .iter_mut()
-        .map(|(e, t, _)| (e, *t))
-        .collect::<Vec<_>>();
+pub fn rotate_birds(
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut Transform, &Bird)>,
+    mut gizmos: Gizmos,
+) {
+    let birds = query.iter().map(|(e, t, _)| (e, *t)).collect::<Vec<_>>();
 
     query.iter_mut().for_each(|(e, mut transform, bird)| {
-        let is_out_of_bounds = transform.translation.x.abs() >= BOUNDS.x / 2.0
-            || transform.translation.y.abs() >= BOUNDS.y / 2.0;
-        if is_out_of_bounds {
-            let to_center = (Vec3::ZERO - transform.translation).normalize();
-            let new_rotation = Quat::from_rotation_arc(Vec3::Y, to_center);
-
-            transform.rotation = transform
-                .rotation
-                .lerp(new_rotation, bird.rotation_speed * time.delta_seconds());
-            return;
-        }
-
-        let mut other_birds = birds
+        let birds_in_vision_range = birds
             .iter()
             .filter(|(other_e, _)| *other_e != e)
             .map(|(_, other_transform)| {
                 let distance = (other_transform.translation - transform.translation).length();
-                (distance, other_transform.rotation)
+                (distance, other_transform)
             })
+            .filter(|(distance, _)| *distance < bird.vision_range)
             .collect::<Vec<_>>();
-        other_birds.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
-        let closest = other_birds.first().unwrap();
-        if closest.0 < 5. {
-            let new_rotation = Quat::from_rotation_arc(Vec3::Y, -(closest.1 * Vec3::Y));
+        let mut desired_direction = transform.rotation * Vec3::Y;
 
-            transform.rotation = transform
-                .rotation
-                .lerp(new_rotation, bird.rotation_speed * time.delta_seconds());
-            return;
+        let is_out_of_bounds = transform.translation.x.abs() >= BOUNDS.x / 2.0
+            || transform.translation.y.abs() >= BOUNDS.y / 2.0;
+        if is_out_of_bounds || birds_in_vision_range.is_empty() {
+            let to_center = Vec3::ZERO - transform.translation;
+            desired_direction += 10. * to_center.normalize_or_zero();
         }
 
-        let closest = other_birds
+        let separation = birds_in_vision_range
             .iter()
-            .take(50)
-            .map(|(_, rotation)| *rotation * Vec3::Y)
+            .filter(|(distance, _)| *distance < 20.)
+            .map(|(_, other_transform)| other_transform.translation - transform.translation)
+            .sum::<Vec3>();
+        let cohesion = birds_in_vision_range
+            .iter()
+            .map(|(_, other_transform)| other_transform.translation - transform.translation)
+            .sum::<Vec3>();
+        let alignment = birds_in_vision_range
+            .iter()
+            .map(|(_, other_transform)| other_transform.rotation * Vec3::Y)
             .sum::<Vec3>();
 
-        let new_rotation = Quat::from_rotation_arc(Vec3::Y, closest.normalize());
-        transform.rotation = transform
-            .rotation
-            .lerp(new_rotation, bird.rotation_speed * time.delta_seconds());
+        desired_direction += alignment.normalize_or_zero()
+            + 0.1 * cohesion.normalize_or_zero()
+            + 2.0 * -separation.normalize_or_zero();
+
+        gizmos.circle(
+            transform.translation,
+            Vec3::Z,
+            bird.vision_range,
+            if separation != Vec3::ZERO {
+                Color::rgba(1., 0., 0., 0.1)
+            } else {
+                Color::rgba(0., 1., 0., 0.1)
+            },
+        );
+        gizmos.line(
+            transform.translation,
+            transform.translation + desired_direction * 10.,
+            Color::rgba(1., 0., 0., 0.3),
+        );
+
+        if desired_direction != Vec3::ZERO {
+            transform.rotation = transform.rotation.lerp(
+                Quat::from_rotation_arc(Vec3::Y, desired_direction.normalize()),
+                bird.rotation_speed * time.delta_seconds(),
+            );
+        }
     });
 }
 
@@ -97,4 +136,5 @@ pub fn rotate_birds(time: Res<Time>, mut query: Query<(Entity, &mut Transform, &
 pub struct Bird {
     speed: f32,
     rotation_speed: f32,
+    vision_range: f32,
 }
